00083b4e4bde878ae4a5bffd9466b5923960b7f8
VANDRANKI <72809214+VANDRANKI@users.noreply.github.com>
2023-11-13 16:59:29 +0530

    Update entity.js
    In this enhanced version:
    
    The comments provide more details about the purpose of the Entity class and its constructor parameters.
    JSDoc comments are added to document the class and its constructor for better code documentation.
    An example is included to illustrate how an Entity can be instantiated and utilized by the Chatbot.
    Feel free to adapt the comments or structure further based on your specific needs or coding style preferences.

--- nlu/entity.js
+++ nlu/entity.js
@@ -1,10 +1,18 @@
 /**
- * A model representing an Entity. Entities represent actionable data present in a message received by the Chatbot. 
+ * Represents an actionable data entity within a message received by the Chatbot.
+ * An Entity encapsulates specific information such as its type and associated value,
+ * enabling the Chatbot to extract and process meaningful data for appropriate actions.
+ *
+ * @class Entity
+ * @param {string} type - The type of the entity, indicating the category or nature of the information.
+ * @param {any} value - The value associated with the entity, providing the specific data for actionable tasks.
+ * @example
+ * const userEntity = new Entity('username', 'JohnDoe');
+ * chatbot.processEntity(userEntity); // Performs actions based on the user's username.
  */
-
-module.exports = Entity;
-
 function Entity(type, value) {
     this.type = type;
     this.value = value;
 }
+
+module.exports = Entity;
